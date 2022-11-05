f829886e859d1882efa71f098f4fd0060f0a764a
Jimmy <1064425721@qq.com>
2022-11-05 12:35:49 +0800

    feat(proxy):使用`proxy`实现双向绑定

--- src/data/vue3/proxy.ts
+++ src/data/vue3/proxy.ts
@@ -1,3 +1,28 @@
-export const proxy = () => {
-  console.log(1);
+/**
+ * @desc proxy 代理实现双向绑定
+ * @param target {Record<string, any>} 需要代理的对象
+ * @param dom {HTMLElement | null }更新dom操作
+ */
+export const proxy = (
+  target: Record<string, any>,
+  dom: HTMLElement | null
+): Record<string, any> => {
+  // 创建proxy实例并返回
+  return new Proxy(target, {
+    // 当被读取时触发：返回访问的代理的对象某个值 target={a:'1',b:'2'},
+    get: (target: Record<string, any>, key: string, receiver: any) => {
+      return target[key];
+    },
+    // 当被重新赋值时触发:将代理对象重新赋值到传入的新值完成数据更新
+    set: (
+      target: Record<string, any>,
+      key: string,
+      newValue: any,
+      receiver: any
+    ) => {
+      dom!.innerHTML = newValue;
+      target[key] = newValue;
+      return true;
+    },
+  });
 };
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,8 @@
 import { proxy } from "@/data/vue3/proxy";
-proxy();
+const proxyInnerHTML = document.getElementById("proxy");
+const proxyInput = document.getElementById("proxyInput");
+const data = proxy({ a: 11 }, proxyInnerHTML);
+proxyInput!.oninput = function (e: any) {
+  const value = e.target.value;
+  data.a = value;
+};
